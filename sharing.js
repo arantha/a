9a067e960421d50c49e173a93749b397617834a9
arantha <arantha.1st@gmail.com>
2017-06-03 19:44:53 +0800

    Create sharing.js

---
+++ sharing.js
@@ -0,0 +1,4 @@
+document.getElementById("toursharing1").innerHTML = 4;
+document.getElementById("toursharing2").innerHTML = 2;
+document.getElementById("toursharing3").innerHTML = 1;
+document.getElementById("toursharing4").innerHTML = 5;
